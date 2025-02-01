1409cbce453bf9c8d57cd26f2524e54e2f38d1b6
altamarluis <laltamar@unal.edu.co>
2025-02-01 17:42:49 -0500

    try2

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -7,41 +7,45 @@ Escriba el codigo que ejecute la accion solicitada en cada pregunta.
 import pandas as pd
 import re
 
-def process_report():
+def pregunta_01():
     def clean_line(line):
-        return re.sub(r'\s+', ' ', line.strip())
+        return re.sub(r'\s+', ' ', line.strip()).replace('.', '').strip()
 
-    def format_header(header):
-        return [col.lower().replace(' ', '_') for col in header]
+    def clean_header(headers):
+        return [header.lower().replace(" ", "_") for header in headers]
 
-    def handle_row(line, current_row):
+    def process_value(line, current_val):
         parts = re.split(r'\s{2,}', line)
         if parts[0].isdigit():
-            if current_row:
-                data.append(current_row[:])
-            current_row.clear()
-            current_row.extend([int(parts[0]), int(parts[1]), float(parts[2].replace(',', '.'))])
-            current_row.append(clean_line(' '.join(parts[3:])))
+            rows.append(current_val[:])
+            current_val.clear()
+            current_val.extend([
+                int(parts[0]),
+                int(parts[1]),
+                float(parts[2].split()[0].replace(',', '.'))
+            ])
+            percent_idx = line.find('%')
+            current_val.append(clean_line(line[percent_idx + 1:]))
         else:
-            current_row[-1] += ' ' + clean_line(line)
+            current_val[-1] += " " + clean_line(line)
 
-    with open('files/input/clusters_report.txt') as file:
-        lines = [line.strip() for line in file.readlines() if '---' not in line]
+    with open("files/input/clusters_report.txt") as f:
+        lines = [line.strip() for line in f.readlines() if "---" not in line]
 
-    header = re.split(r'\s{2,}', lines[0])
-    header = format_header(header)
+    header = re.split(r"\s{2,}", lines[0])
+    header[1] += " palabras clave"
+    header[2] += " palabras clave"
 
-    data = []
-    current_row = []
+    rows = []
+    current_val = header
 
     for line in lines[2:]:
-        handle_row(line, current_row)
+        if line:
+            process_value(line, current_val)
 
-    if current_row:
-        data.append(current_row)
+    rows.append(current_val)
+    rows[0] = clean_header(rows[0])
 
-    df = pd.DataFrame(data, columns=header)
+    df = pd.DataFrame(data=rows[1:], columns=rows[0])
     return df
 
-df = process_report()
-print(df)
